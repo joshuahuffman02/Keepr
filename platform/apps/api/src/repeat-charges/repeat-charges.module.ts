54f153c97aea29492fa507d4a30ce733f6222396
joshuahuffman02 <129413071+joshuahuffman02@users.noreply.github.com>
2025-12-19 18:45:58 -0600

    fix: break repeat charges module cycle

--- platform/apps/api/src/repeat-charges/repeat-charges.module.ts
+++ platform/apps/api/src/repeat-charges/repeat-charges.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { RepeatChargesService } from './repeat-charges.service';
 import { RepeatChargesController } from './repeat-charges.controller';
 import { PrismaService } from '../prisma/prisma.service';
-import { ReservationsModule } from '../reservations/reservations.module';
 import { SeasonalRatesModule } from '../seasonal-rates/seasonal-rates.module';
 
 @Module({
-    imports: [ReservationsModule, SeasonalRatesModule],
+    imports: [SeasonalRatesModule],
     controllers: [RepeatChargesController],
     providers: [RepeatChargesService],
     exports: [RepeatChargesService],
